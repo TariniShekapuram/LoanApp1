4f1c336c7fe06d54d1735869facdddac5c7b2c55
Tarini <shekapuramtarini@gmail.com>
2021-06-09 10:45:44 +0530

    initial commit

--- app.py
+++ app.py
@@ -13,18 +13,7 @@ from wtforms.validators import InputRequired,Length,Email
 from werkzeug.security import generate_password_hash,check_password_hash
 
 
-app = Flask(__name__, static_url_path='/static')
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 app.config['SECRET_KEY']="myloanapp"
